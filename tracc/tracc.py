5929ee2eced7ad1ebdda411b82fb0ef6b41d4bac
GraeWolf <kmccuddy@protonmail.com>
2023-12-08 17:02:39 -0500

    testing StatMenu

--- tracc/tracc.py
+++ tracc/tracc.py
@@ -8,9 +8,11 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QIcon
 
 from win_menu import WinMenu
+from stat_win import StatMenu
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    win_menu = WinMenu()
+    win_menu = StatMenu()
     win_menu.show()
     sys.exit(app.exec())
